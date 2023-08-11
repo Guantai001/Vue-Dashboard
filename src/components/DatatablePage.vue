4d3004a58724e56721adcf859d304519b4becb74
GUANTAI001 <jpguantai@gmail.com>
2023-08-11 22:57:42 +0300

    worked on userdata

---
+++ src/components/DatatablePage.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="relative mt-10">
+    <table class="w-full text-sm text-left text-gray-500 ">
+        <thead class="text-xs text-gray-700 uppercase ">
+            <tr>
+            <th scope="col" class="px-6 py-3">
+                  User
+                </th>
+                <th scope="col" class="px-6 py-3">
+                   Username/USSD
+                </th>
+                <th scope="col" class="px-6 py-3">
+                   Access
+                </th>
+                 <th scope="col" class="px-6 py-3">
+                   Group
+                </th>
+                 <th scope="col" class="px-6 py-3">
+                   Status
+                </th>
+                 <th scope="col" class="px-6 py-3">
+                   Action
+                </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white border-b " v-for="user in paginatedUsers" :key="user.id">
+              <td class="px-6 py-4">{{ user.name }}</td>
+              <td class="px-6 py-4">{{ user.username }}</td>
+              <td class="px-6 py-4">{{ user.access }}</td>
+               <td class="px-6 py-4">{{ user.group }}</td>
+                <td class="px-6 py-4">{{ user.status }}</td>
+                <td class="px-6 py-4">{{ user.action }}</td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+         <div class='flex m-4 justify-end'>
+         <div class='flex  justify-between mx-20'>
+        <p class="p-2">Show</p>
+          <select class="p-2 bg-white border" v-model="itemsPerPage" @change="changeItemsPerPage">
+          <option v-for="option in itemsPerPageOptions" :value="option" :key="option">{{ option }}</option>
+        </select> 
+        <p class="p-2">Entries</p>
+        </div>
+       <nav aria-label="Page navigation">
+          <ul class="pagination flex mt-2 mx-4">
+            <li class="page-item mx-2">
+              <a class="page-link" href="#" @click.prevent="prevPage">Previous</a>
+            </li>
+            <li class="page-item mx-2" v-for="page in [currentPage]" :key="page" :class="{ 'active': currentPage === page }">
+              <a class="page-link" href="#" @click.prevent="gotoPage(page)">{{ page }}</a>
+            </li>
+            <li class="page-item mx-2">
+              <a class="page-link" href="#" @click.prevent="nextPage">Next</a>
+            </li>
+          </ul>
+        </nav>
+        </div>
+
+</template>
+
+
+<script>
+
+import {users} from './userData.js:'
+
+export default {
+  data() {
+    return {
+      users: users ,
+      itemsPerPageOptions: [5, 10, 20],
+      itemsPerPage: 5,
+      currentPage: 1,
+    };
+  },
+  computed: {
+    paginatedUsers() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.users.slice(startIndex, endIndex);
+    },
+    totalPages() {
+      return Math.ceil(this.users.length / this.itemsPerPage);
+    },
+  },
+  methods: {
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    gotoPage(page) {
+      this.currentPage = page;
+    },
+    changeItemsPerPage() {
+      this.currentPage = 1; 
+    },
+  },
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex justify-between  mt-5">
+    <div class="flex flex-col">
+      <h1 class="text-2xl font-bold text-black">User Listing</h1>
+      <p class="text-lg font-regular text-grey">User Listing</p>
+    </div>
+    <input 
+        type="text" 
+        class="h-10 px-3 pr-8 text-sm text-grey-darker border rounded ountline-none mt-5 focus:outline-none focus:shadow-outline" 
+        placeholder="Search by name"
+    />
+  </div>
+  <div class="flex mt-10 justify-between">
+  <div class="flex ">
+  <select class="block appearance-none bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded  mx-4 leading-tight focus:outline-none focus:shadow-outline">
+    <option>Sort by</option>
+    <option>Ascending</option>
+    <option>Descending</option>
+    </select>
+      <select class="block appearance-none bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded mr-4  leading-tight focus:outline-none focus:shadow-outline">
+    <option>Sort by</option>
+    <option>Ascending</option>
+    <option>Descending</option>
+    </select>
+      <select class="block appearance-none bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded  leading-tight focus:outline-none focus:shadow-outline">
+    <option>Sort by</option>
+    <option>Ascending</option>
+    <option>Descending</option>
+    </select>
+    </div>
+      <div class="flex ">
+  <select class="block appearance-none bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded  mx-4 leading-tight focus:outline-none focus:shadow-outline">
+    <option>Sort by</option>
+    <option>Ascending</option>
+    <option>Descending</option>
+    </select>
+    <select class="block appearance-none bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded  mx-4 leading-tight focus:outline-none focus:shadow-outline">
+    <option>Sort by</option>
+    <option>Ascending</option>
+    <option>Descending</option>
+    </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserListing'
+};
+</script>
+
+<style scoped>
+/* Add your scoped styles here */
+</style>
